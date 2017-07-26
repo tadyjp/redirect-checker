385e650f21f286db39fcb8667ba197c5d773c786
Masato TADA <a.dat.jp@gmail.com>
2017-07-26 16:55:22 +0900

    curl globoff

--- checker.sh
+++ checker.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -eu
+# set -eu
 
 BASEDIR=$(dirname "$0")
 URL_LIST="$BASEDIR/urls.txt"
@@ -8,9 +8,9 @@ URL_LIST="$BASEDIR/urls.txt"
 cat ${URL_LIST} | while read line
 do
   row=(`echo ${line}`)
-  redirect_url=(`curl -I -s -o /dev/null ${row[0]} -w '%{redirect_url}'`)
+  redirect_url=(`curl -I --globoff -s -o /dev/null "${row[0]}" -w '%{redirect_url}'`)
 
-  if [ ${redirect_url} = ${row[1]} ] ; then
+  if [ "${redirect_url}" = "${row[1]}" ] ; then
     echo "OK: ${row[0]} -> ${redirect_url}"
   else
     echo "NG: ${row[0]} -> ${redirect_url} != ${row[1]}"
